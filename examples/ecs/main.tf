eb33e0f1d2bd798605a5a2813354d2c26b8ccfab
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-05-20 01:41:29 -0400

    feat: update example

--- examples/ecs/main.tf
+++ examples/ecs/main.tf
@@ -1,28 +1,76 @@
-module "ceramic-ecs" {
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "cloudflare" {
+  # api_token = var.cloudflare_api_token # This can also be specified with the CLOUDFLARE_API_TOKEN shell environment variable.
+}
+
+provider "docker" {
+  host = "http://registry.hub.docker.com"
+}
+
+module "ceramic_ecs" {
   source = "../../modules/ecs"
 
-  env                   = var.env
-  aws_region            = var.aws_region
-  az_count              = var.az_count
-  private_subnet_ids    = var.private_subnet_ids
-  public_subnet_ids     = var.public_subnet_ids
-  ssl_certificate_arn   = var.ssl_certificate_arn
-  vpc_security_group_id = var.vpc_security_group_id
-  vpc_id                = var.vpc_id
-  vpc_cidr_block        = var.vpc_cidr_block
-  ecs_cluster_name      = var.ecs_cluster_name
-  image_tag             = var.image_tag
-  base_namespace        = var.base_namespace
-  default_tags          = var.default_tags
+  count = var.deployments
 
-  ceramic_cors_allowed_origins   = var.ceramic_cors_allowed_origins
-  ceramic_eth_rpc_url            = var.ceramic_eth_rpc_url
+  acm_certificate_arn            = data.aws_acm_certificate._3boxlabs_com.arn
+  aws_region                     = var.aws_region
+  base_namespace                 = "${var.cohort}-${count.index + 1}"
   ceramic_anchor_service_api_url = var.ceramic_anchor_service_api_url
-  ceramic_efs_logs_fs_id         = var.ceramic_efs_logs_fs_id
-  ceramic_efs_logs_volume_name   = var.ceramic_efs_logs_volume_name
+  ceramic_cors_allowed_origins   = var.ceramic_cors_allowed_origins
   ceramic_cpu                    = var.ceramic_cpu
+  ceramic_eth_rpc_url            = data.aws_ssm_parameter.eth_rpc_url.value
+  ceramic_efs_logs_fs_id         = var.ceramic_efs_logs_fs_id
+  ceramic_efs_logs_fs_name       = var.ceramic_efs_logs_fs_name
+  ceramic_memory                 = var.ceramic_memory
+  ceramic_network                = var.ceramic_network
   ceramic_task_count             = var.ceramic_task_count
+  ecs_cluster_name               = var.ecs_cluster_name
+  env                            = var.env
+  default_tags                   = var.default_tags
+  image_tag                      = var.image_tag
+  ipfs_cpu                       = var.ipfs_cpu
+  ipfs_debug_env_var             = var.ipfs_debug_env_var
+  ipfs_domain_name               = var.domain_name
+  ipfs_memory                    = var.ipfs_memory
+  ipfs_task_count                = var.ipfs_task_count
+  private_subnet_ids             = data.aws_subnet_ids.private.ids
+  public_subnet_ids              = data.aws_subnet_ids.public.ids
+  s3_bucket_name                 = var.s3_bucket_name
+  vpc_cidr_block                 = data.aws_vpc.main.cidr_block
+  vpc_id                         = var.vpc_id
+  vpc_security_group_id          = var.vpc_security_group_id
+}
 
-  ipfs_task_count = var.ipfs_task_count
-  ipfs_ecs_memory = var.ipfs_ecs_memory
+data "aws_acm_certificate" "_3boxlabs_com" {
+  domain      = "*.${var.domain_name}"
+  most_recent = true
+}
+
+data "aws_ssm_parameter" "eth_rpc_url" {
+  name = "/ceramic-${var.ceramic_env}/${lookup(var.eth_network, var.ceramic_env)}/infura_rpc_endpoint"
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Ceramic = var.ceramic_env
+    Subnet  = "private"
+  }
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Ceramic = var.ceramic_env
+    Subnet  = "public"
+  }
+}
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
 }
