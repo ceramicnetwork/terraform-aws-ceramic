f00df752abb3b7e494eac8f99f74d5e51b1cc0f9
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-05-20 01:39:05 -0400

    fix: update lb s3 and acm cert

--- modules/ecs/ceramic/storage.tf
+++ modules/ecs/ceramic/storage.tf
@@ -5,7 +5,7 @@ module "s3_alb" {
   create_bucket = true
 
   # only lowercase alphanumeric characters and hyphens allowed
-  bucket = "${var.namespace}-alb"
+  bucket = "${local.namespace}-alb"
   acl    = "private"
 
   attach_elb_log_delivery_policy = true
@@ -24,33 +24,5 @@ module "s3_alb" {
     }
   ]
 
-  tags = var.default_tags
-}
-
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 1.25.0"
-
-  create_bucket = true
-
-  bucket = "${local.namespace}-module"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
-  lifecycle_rule = [
-    {
-      enabled = true
-
-      noncurrent_version_expiration = {
-        days = 30
-      }
-    }
-  ]
-}
-
-data "aws_s3_bucket" "alb" {
-  bucket = module.s3_alb.this_s3_bucket_id
+  tags = local.default_tags
 }
