032d8de730aafcbfdde4b2bbc2ea33fde5630225
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-03-12 18:16:49 -0500

    fix(ecs-ipfs): add alb logging var

--- modules/ecs/ipfs/variables.tf
+++ modules/ecs/ipfs/variables.tf
@@ -116,6 +116,11 @@ variable "vpc_security_group_id" {
 
 /* Specified */
 
+variable "enable_alb_logging" {
+  type        = bool
+  description = "True to create an S3 bucket to store ALB logs"
+}
+
 variable "enable_external_api" {
   type        = bool
   description = "True to enable IPFS API on external ALB"
