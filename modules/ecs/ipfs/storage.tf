a048c6898bed8a7d9cd84b4305e4d2dfacabebee
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-03-12 18:09:34 -0500

    feat(ecs-ipfs): optional alb logs

--- modules/ecs/ipfs/storage.tf
+++ modules/ecs/ipfs/storage.tf
@@ -2,7 +2,7 @@ module "s3_alb" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "1.25.0"
 
-  create_bucket = true
+  create_bucket = var.enable_alb_logging
 
   bucket = "${local.namespace}-alb.logs"
   acl    = "private"
