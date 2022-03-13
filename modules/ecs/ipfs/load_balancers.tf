a57a73cfda9be1dd65048b100b115a94074b2155
stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-03-13 19:29:12 -0400

    feat(ecs-ipfs): keep listener for insecure swarm

--- modules/ecs/ipfs/load_balancers.tf
+++ modules/ecs/ipfs/load_balancers.tf
@@ -125,7 +125,7 @@ resource "aws_lb_target_group" "gateway" {
 /* Swarm Websocket */
 
 resource "aws_lb_listener" "swarm_ws_http" {
-  count = ! var.use_ssl ? 1 : 0
+  count = 1
 
   load_balancer_arn = aws_lb.external.arn
   port              = local.swarm_ws_port
@@ -141,13 +141,13 @@ resource "aws_lb_listener" "swarm_ws_https" {
   count = var.use_ssl ? 1 : 0
 
   load_balancer_arn = aws_lb.external.arn
-  port              = local.swarm_ws_port
+  port              = local.swarm_wss_port
   protocol          = "HTTPS"
   certificate_arn   = var.acm_certificate_arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.swarm_ws.arn
+    target_group_arn = aws_lb_target_group.swarm_wss.arn
   }
 }
 
@@ -156,6 +156,27 @@ resource "aws_lb_target_group" "swarm_ws" {
   name_prefix = "swws-"
   protocol    = "HTTP"
   port        = local.swarm_ws_port
+  target_type = "ip"
+  health_check {
+    interval            = 60
+    timeout             = 30
+    path                = "/"
+    port                = local.healthcheck_port
+    matcher             = "200"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = local.default_tags
+
+  depends_on = [aws_lb.external]
+}
+
+resource "aws_lb_target_group" "swarm_wss" {
+  vpc_id      = var.vpc_id
+  name_prefix = "swwss-"
+  protocol    = "HTTP"
+  port        = local.swarm_wss_port
   target_type = "ip"
   health_check {
     interval            = 60
@@ -230,6 +251,20 @@ module "alb_internal" {
         healthy_threshold   = 2
         unhealthy_threshold = 3
       }
+    },
+    {
+      name_prefix      = "swwss-"
+      backend_protocol = "HTTP"
+      backend_port     = local.swarm_wss_port
+      target_type      = "ip"
+      health_check = {
+        interval            = 30
+        path                = "/"
+        port                = local.healthcheck_port
+        matcher             = "200"
+        healthy_threshold   = 2
+        unhealthy_threshold = 3
+      }
     }
   ]
 
@@ -252,7 +287,14 @@ module "alb_internal" {
       target_group_index = 2
       action_type        = "forward"
     }
-  ] : []
+  ] : [
+    {
+      port               = local.swarm_ws_port
+      protocol           = "HTTP"
+      target_group_index = 2
+      action_type        = "forward"
+    }
+  ]
 
   https_listeners = var.use_ssl ? [
     {
@@ -268,10 +310,10 @@ module "alb_internal" {
       target_group_index = 1
     },
     {
-      port               = local.swarm_ws_port
+      port               = local.swarm_wss_port
       protocol           = "HTTPS"
       certificate_arn    = var.acm_certificate_arn
-      target_group_index = 2
+      target_group_index = 3
     }
   ] : []
 
