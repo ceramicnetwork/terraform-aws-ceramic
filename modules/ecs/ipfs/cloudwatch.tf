dbd5509a3ca7b560675e720dcf1fcf336d4e201d
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-05-24 10:28:24 -0400

    fix(ecs-ipfs): get default tags from locals

--- modules/ecs/ipfs/cloudwatch.tf
+++ modules/ecs/ipfs/cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "data_sync" {
   count             = var.enable_repo_backup_to_s3 ? 1 : 0
   name              = "/aws/datasync/${local.namespace}"
   retention_in_days = 1827
-  tags              = var.default_tags
+  tags              = local.default_tags
 
   lifecycle {
     prevent_destroy = true
